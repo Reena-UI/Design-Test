e71322aa1ea7100801d6b46ddec0bc440198dab5
Reena Jakashaniya <Reena@fourmangos.com>
2020-08-05 17:58:53 +0530

    Slider

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
 
         <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script type="text/javascript" src="{{ asset('slick/slick.min.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}" defer></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -17,6 +19,14 @@
     <body>
         @include('layouts.inc.nav')
         @yield('content')
+        <div data-slick='{"slidesToShow": 4, "slidesToScroll": 4}' class="slider">
+            <div><h3>1</h3></div>
+            <div><h3>2</h3></div>
+            <div><h3>3</h3></div>
+            <div><h3>4</h3></div>
+            <div><h3>5</h3></div>
+            <div><h3>6</h3></div>
+          </div>
         @include('layouts.inc.footer')
     </body>
 </html>
--- resources/views/layouts/inc/nav.blade.php
+++ resources/views/layouts/inc/nav.blade.php
@@ -1,13 +1,13 @@
  <!-- Navigation-->
 <header class="header">
-<div class="header-top py-2">
+<div class="header-top py-1">
 <div class="container">
 <div class="row">
-<div class="col-md-6">
-<i class="fa fa-phone mr-1"></i> <span class="d-none d-sm-inline-block">Need Help? Free call us 24/7 :</span> <a href="tel:(+1) 00 123 456">(+1) 00 123 456 </a>
+<div class="col-6">
+<i class="fa fa-phone mr-1"></i> <span class="d-none d-sm-inline-block">Need Help? Free call us 24/7 :</span> <a href="tel:(+1) 00 123 456" class="header-top-link">(+1) 00 123 456 </a>
 </div>
-<div class="col-md-6 text-right">
-    <ul class="list-inline mb-0">
+<div class="col-6 text-right">
+    <ul class="list-inline mb-0 social-list">
         <li class="list-inline-item"><a href=""><i class="fab fa-facebook-f"></i></a></li>
         <li class="list-inline-item"><a href=""><i class="fab fa-instagram"></i></a></li>
         <li class="list-inline-item"><a href=""><i class="fab fa-twitter"></i></a></li>
@@ -18,7 +18,7 @@
 </div>
 <div class="header-middle">
  <!-- Navigation-->
- <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+ <nav class="navbar navbar-expand-lg text-uppercase" id="mainNav">
     <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
         <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
